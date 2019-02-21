2375b5a35a9b195310889246c2f0202549536bc1
wpsmith <t@wpsmith.net>
2019-02-20 20:36:46 -0500

    Update namespace/docs

--- src/images.php
+++ src/images.php
@@ -8,7 +8,7 @@
  *
  * @package    WPS\Functions
  * @author     Travis Smith <t@wpsmith.net>
- * @copyright  2015-2018 Travis Smith
+ * @copyright  2015-2019 Travis Smith
  * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License v2
  * @link       https://github.com/wpsmith/WPS
  * @version    1.0.0
